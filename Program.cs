c24e4ecbc2cc77fe744dbae49146b203625ba3ca
Danro <86835117+rsnlnd@users.noreply.github.com>
2025-09-15 11:32:22 +0200

    Grundlæggende menu system for Hangman spillet.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Projekt_SpilUge38
@@ -17,7 +19,8 @@ namespace Projekt_SpilUge38
                 // Her vises Hovedmenuen som giver 4 forskellige muligheder
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Green; // Sæt tekstfarven til grøn
-                Console.WriteLine("888b     d888        d8888 8888888 888b    888      888b     d888 8888888888 888b    888 888     888 \n8888b   d8888       d88888   888   8888b   888      8888b   d8888 888        8888b   888 888     888 \n88888b.d88888      d88P888   888   88888b  888      88888b.d88888 888        88888b  888 888     888 \n888Y88888P888     d88P 888   888   888Y88b 888      888Y88888P888 8888888    888Y88b 888 888     888 \n888 Y888P 888    d88P  888   888   888 Y88b888      888 Y888P 888 888        888 Y88b888 888     888 \n888  Y8P  888   d88P   888   888   888  Y88888      888  Y8P  888 888        888  Y88888 888     888 \n888   \"   888  d8888888888   888   888   Y8888      888   \"   888 888        888   Y8888 Y88b. .d88P \n888       888 d88P     888 8888888 888    Y888      888       888 8888888888 888    Y888  \"Y88888P\"  \n                                                                                                     \n                                                                                                     \n                                                                                                     \n");
+                Console.WriteLine("\r\n███╗░░░███╗░█████╗░██╗███╗░░██╗  ███╗░░░███╗███████╗███╗░░██╗██╗░░░██╗\r\n████╗░████║██╔══██╗██║████╗░██║  ████╗░████║██╔════╝████╗░██║██║░░░██║\r\n██╔████╔██║███████║██║██╔██╗██║  ██╔████╔██║█████╗░░██╔██╗██║██║░░░██║\r\n██║╚██╔╝██║██╔══██║██║██║╚████║  ██║╚██╔╝██║██╔══╝░░██║╚████║██║░░░██║\r\n██║░╚═╝░██║██║░░██║██║██║░╚███║  ██║░╚═╝░██║███████╗██║░╚███║╚██████╔╝\r\n╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝╚═╝░░╚══╝  ╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚══╝░╚═════╝░\n\n");
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("CHOOSE A GAME\n");
                 Console.WriteLine("1. Rock, Paper, Scissor");
                 Console.WriteLine("2. Hangman");
@@ -59,8 +62,40 @@ namespace Projekt_SpilUge38
         static void Hangman() // Hangman spiller
         {
             Console.Clear();
-            Console.WriteLine("Game started!");
-            Console.WriteLine("Hangman spil");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\r\n█░█ ▄▀█ █▄░█ █▀▀ █▀▄▀█ ▄▀█ █▄░█\r\n█▀█ █▀█ █░▀█ █▄█ █░▀░█ █▀█ █░▀█\n\n");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("1. Start game");
+            Console.WriteLine("2. Game rules");
+            Console.WriteLine("3. Back to main menu");
+
+            string drValg = Console.ReadKey().KeyChar.ToString(); // Spillerens valg
+
+            switch (drValg)
+            {
+                case "1":
+                    Console.WriteLine("\nStart game");
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\r\n█▀▀ ▄▀█ █▀▄▀█ █▀▀   █▀█ █░█ █░░ █▀▀ █▀\r\n█▄█ █▀█ █░▀░█ ██▄   █▀▄ █▄█ █▄▄ ██▄ ▄█\n\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Et hemmeligt ord bliver valgt automatisk.\n");
+                        Console.WriteLine("Du kan se, hvor mange bogstaver ordet har – hvert bogstav vises som en streg '_'.\n");
+                        Console.WriteLine("Gæt ét bogstav ad gangen.\n - Rigtigt bogstav > det bliver vist på sin plads i ordet.\n - Forkert bogstav → du mister et liv.\n");
+                        Console.WriteLine("Du starter med 5 liv. Når du mister alle liv, har du tabt.\n");
+                        Console.WriteLine("Hvis du gætter hele ordet, vinder du spillet.\n");
+                        Console.WriteLine("Når spillet er slut, kan du vælge at prøve igen eller gå tilbage til menuen.\n");
+                    break;
+                case "3":
+                    Console.WriteLine("\nBack to main menu");
+                    break;
+            }
+
+            Console.WriteLine("< PRESS ANY KEY TO GO BACK");
+
             Console.ReadKey();
         }
     }
