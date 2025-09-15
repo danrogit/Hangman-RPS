688f90154a3f05905db8b82b759866cbb6a916ba
Casper Christensen <Casper@perb.dk>
2025-09-15 09:50:36 +0200

    Start menu, sten saks papir - hangman. Byg grundbasen for Hangman og Sten Saks Papir. Opsætning samt første step til valg af menu.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,37 @@ namespace Projekt_SpilUge38
         static void Main(string[] args)
         {
 
+            bool running = true;
+            while (running)
 
-        }
-    }
+
+            {
+                // Her vises "Hovedmenuen som giver 3 forskellige muligheder
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green; // Sæt tekstfarven til grøn
+                Console.WriteLine("=== Hovedmenu ===");
+                Console.WriteLine("1. Start game");
+                Console.WriteLine("2. Rules");
+                Console.WriteLine("3. Quit game");
+
+                string valg = Console.ReadLine(); // Her ventes der på hvad spilleren tager af valg
+
+                switch (valg) // Vælger mellem flere kodeblokker, baseret på værdien.
+                {
+                    case "1":
+                        StartGame();
+                        break;
+                    case "2":
+                        Console.WriteLine("Regler: Sten slår saks, saks slår papir, papir slår sten");
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        running = false;
+                        Console.WriteLine("Quit game");
+                        break;
+
+
+
+                }
+            }
 }
