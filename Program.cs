b3fba9df5c6cec801fdc2adccae957814bd6614f
Danro <86835117+rsnlnd@users.noreply.github.com>
2025-09-15 10:26:22 +0200

    Opdateret hovedmenu med to funktioner til hvert spil.

--- Program.cs
+++ Program.cs
@@ -13,35 +13,57 @@ namespace Projekt_SpilUge38
 
             bool running = true;
             while (running)
-
-
             {
-                // Her vises "Hovedmenuen som giver 3 forskellige muligheder
+                // Her vises Hovedmenuen som giver 4 forskellige muligheder
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Green; // Sæt tekstfarven til grøn
-                Console.WriteLine("=== Hovedmenu ===");
-                Console.WriteLine("1. Start game");
-                Console.WriteLine("2. Rules");
-                Console.WriteLine("3. Quit game");
+                Console.WriteLine("888b     d888        d8888 8888888 888b    888      888b     d888 8888888888 888b    888 888     888 \n8888b   d8888       d88888   888   8888b   888      8888b   d8888 888        8888b   888 888     888 \n88888b.d88888      d88P888   888   88888b  888      88888b.d88888 888        88888b  888 888     888 \n888Y88888P888     d88P 888   888   888Y88b 888      888Y88888P888 8888888    888Y88b 888 888     888 \n888 Y888P 888    d88P  888   888   888 Y88b888      888 Y888P 888 888        888 Y88b888 888     888 \n888  Y8P  888   d88P   888   888   888  Y88888      888  Y8P  888 888        888  Y88888 888     888 \n888   \"   888  d8888888888   888   888   Y8888      888   \"   888 888        888   Y8888 Y88b. .d88P \n888       888 d88P     888 8888888 888    Y888      888       888 8888888888 888    Y888  \"Y88888P\"  \n                                                                                                     \n                                                                                                     \n                                                                                                     \n");
+                Console.WriteLine("CHOOSE A GAME\n");
+                Console.WriteLine("1. Rock, Paper, Scissor");
+                Console.WriteLine("2. Hangman");
+                Console.WriteLine("3. Credits");
+                Console.WriteLine("4. Quit");
 
                 string valg = Console.ReadLine(); // Her ventes der på hvad spilleren tager af valg
 
                 switch (valg) // Vælger mellem flere kodeblokker, baseret på værdien.
                 {
                     case "1":
-                        StartGame();
+                        RPS();
                         break;
                     case "2":
-                        Console.WriteLine("Regler: Sten slår saks, saks slår papir, papir slår sten");
+                        Hangman();
                         Console.ReadKey();
                         break;
                     case "3":
                         running = false;
-                        Console.WriteLine("Quit game");
+                        Console.WriteLine("Credits");
                         break;
-
-
-
+                    case "4":
+                        running = false;
+                        Console.WriteLine("Quit");
+                        break;
                 }
             }
+        }
+
+        static void RPS() // Sten saks papir
+        {
+            
+            Console.Clear();
+            Console.WriteLine("RPS");
+            Console.WriteLine("RPS spil");
+            Console.ReadKey();
+        }
+
+        static void Hangman() // Hangman spiller
+        {
+            Console.Clear();
+            Console.WriteLine("Game started!");
+            Console.WriteLine("Hangman spil");
+            Console.ReadKey();
+        }
+    }
 }
+
+
